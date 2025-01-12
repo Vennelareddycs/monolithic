d203a5962e078040ba533050baf91812e8df3e81
Vennela <96403605+Vennelareddycs@users.noreply.github.com>
2025-01-12 18:06:27 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "one" {
-  bucket = "devops.project.bucket"
+  bucket = "project.bucket"
 }
 
 resource "aws_s3_bucket_ownership_controls" "two" {
@@ -23,10 +23,5 @@ status = "Enabled"
 }
 }
 
-terraform {
-backend "s3" {
-region = "us-east-1"
-bucket = "devops.project.bucket"
-key = "prod/terraform.tfstate"
-}
+
 }
